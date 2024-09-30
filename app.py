aed5cd172baf96e41fa92265ba8773646d97d7a0
desciphear <167032565+desciphear@users.noreply.github.com>
2024-09-30 12:25:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -72,9 +72,10 @@ def analyzer():
 
         #Taking output as String
         txt = sentimentAnalysis.sentiment
+        score = sentimentAnalysis.confidence_scores
          
         #Printing Final Output
-        output = 'Sentiment of Given Speech is ' + txt
+        output = 'Sentiment of Given Speech is ' + txt +" with confidence scores: \n"+f"\n Positive: {score.positive:.2f} \n Negative: {score.negative:.2f} \n Neutral: {score.neutral:.2f}"
     else:
         
         output = 'No Input Provided by the user'
